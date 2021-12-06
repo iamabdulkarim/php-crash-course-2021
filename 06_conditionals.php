ef3deac0074466d7d990acc37be49eb98c51c5c9
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-12-06 17:16:20 +0600

    conditionals if|else|switch

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -1,10 +1,13 @@
 <?php
 
-$age = 20;
+$number = 0;
 $salary = 300000;
 
 // Sample if
-
+if($number === 20){
+    echo ' 1';
+}
+echo '<br>';
 // Without circle braces
 
 // Sample if-else
@@ -16,9 +19,29 @@ $salary = 300000;
 // if OR
 
 // Ternary if
-
+echo $number < 22 ? 'young' : 'old';
 // Short ternary
-
+$myNumber = $number ?: 18;
+echo '<pre>';
+var_dump($myNumber);
+echo '</pre>';
 // Null coalescing operator
-
+$newName = isset($name) ? $name :'karim';
+$newName = $name ?? 'karim';
 // switch
+$userRole = 'karim';
+switch($userRole){
+    case 'admin':
+        echo 'admin';
+        break;
+    case 'editor':
+        echo 'editor';
+        break;
+    case 'user':
+        echo 'user';
+        break;
+
+    default:
+        echo 'Invalid Role';
+    
+}
