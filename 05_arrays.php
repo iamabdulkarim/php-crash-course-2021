a9bebf7c0d187b81630ceddf005e19f293932ddc
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-12-06 15:23:11 +0600

    Create an associative array & Null coalescing assignment operator. Since PHP 7.4

--- 05_arrays.php
+++ 05_arrays.php
@@ -1,90 +1,90 @@
 <?php
 
-// Create array
-$names = ["karim","Noman","Bakkar"];
-// Print the whole array
-echo '<pre>';
-var_dump($names);
-echo '</pre>';
+// // Create array
+// $names = ["karim","Noman","Bakkar"];
+// // Print the whole array
+// echo '<pre>';
+// var_dump($names);
+// echo '</pre>';
 
-// Get element by index
-echo $names[2].'<br>';
-echo $names[1].'<br>';
-echo $names[0].'<br>';
-// Set element by index
-$names[0] = 'Mamshad';
-echo '<pre>';
-echo var_dump($names);
-echo '</pre>';
+// // Get element by index
+// echo $names[2].'<br>';
+// echo $names[1].'<br>';
+// echo $names[0].'<br>';
+// // Set element by index
+// $names[0] = 'Mamshad';
+// echo '<pre>';
+// echo var_dump($names);
+// echo '</pre>';
 
-// Check if array has element at index 2
-isset($names[2]); //true
-isset($names[4]); //false
-// Append element
-$names[] = 'Riaz';
-echo '<pre>';
-echo var_dump($names);
-echo '</pre>';
-// Print the length of the array
-echo count($names).'<br>';
-// Add element at the end of the array
-array_push($names, 'karim');
-echo '<pre>';
-echo var_dump($names);
-echo '</pre>';
-// Remove element from the end of the array
+// // Check if array has element at index 2
+// isset($names[2]); //true
+// isset($names[4]); //false
+// // Append element
+// $names[] = 'Riaz';
+// echo '<pre>';
+// echo var_dump($names);
+// echo '</pre>';
+// // Print the length of the array
+// echo count($names).'<br>';
+// // Add element at the end of the array
+// array_push($names, 'karim');
+// echo '<pre>';
+// echo var_dump($names);
+// echo '</pre>';
+// // Remove element from the end of the array
 
-echo array_pop($names);
-echo '<pre>';
-echo var_dump($names);
-echo '</pre>';
-// Add element at the beginning of the array
-array_unshift($names , 'Rifat');
-echo '<pre>';
-echo var_dump($names);
-echo '</pre>';
-// Remove element from the beginning of the array
-echo array_shift($names);
-echo '<pre>';
-echo var_dump($names);
-echo '</pre>';
-// Split the string into an array
-$versityFriends = "miraz,sajib,arafat";
-echo '<pre>';
-echo var_dump(explode(",",$versityFriends));
-echo '</pre>';
-// Combine array elements into string
-echo '<br>';
+// echo array_pop($names);
+// echo '<pre>';
+// echo var_dump($names);
+// echo '</pre>';
+// // Add element at the beginning of the array
+// array_unshift($names , 'Rifat');
+// echo '<pre>';
+// echo var_dump($names);
+// echo '</pre>';
+// // Remove element from the beginning of the array
+// echo array_shift($names);
+// echo '<pre>';
+// echo var_dump($names);
+// echo '</pre>';
+// // Split the string into an array
+// $versityFriends = "miraz,sajib,arafat";
+// echo '<pre>';
+// echo var_dump(explode(",",$versityFriends));
+// echo '</pre>';
+// // Combine array elements into string
+// echo '<br>';
 
-echo(implode(",",$names));
+// echo(implode(",",$names));
 
-// Check if element exist in the array
-echo '<pre>';
-var_dump(in_array('noman',$names));
-echo '</pre>';
+// // Check if element exist in the array
+// echo '<pre>';
+// var_dump(in_array('noman',$names));
+// echo '</pre>';
 
-// Search element index in the array
-echo '<pre>';
-var_dump(array_search('Bakkar',$names));
-echo '</pre>';
-// Merge two arrays
-$newFriends = ['miraz','alif','sadek'];
-echo '<pre>';
-var_dump([...$names,...$newFriends]);
-echo '</pre>';
-// Sorting of array (Reverse order also)
-echo '<pre>';
-var_dump($newFriends);
-echo '</pre>';
-sort($names);
-echo '<pre>';
-var_dump($names);
-echo '</pre>';
+// // Search element index in the array
+// echo '<pre>';
+// var_dump(array_search('Bakkar',$names));
+// echo '</pre>';
+// // Merge two arrays
+// $newFriends = ['miraz','alif','sadek'];
+// echo '<pre>';
+// var_dump([...$names,...$newFriends]);
+// echo '</pre>';
+// // Sorting of array (Reverse order also)
+// echo '<pre>';
+// var_dump($newFriends);
+// echo '</pre>';
+// sort($names);
+// echo '<pre>';
+// var_dump($names);
+// echo '</pre>';
 
-rsort($newFriends);
-echo '<pre>';
-var_dump($newFriends);
-echo '</pre>';
+// rsort($newFriends);
+// echo '<pre>';
+// var_dump($newFriends);
+// echo '</pre>';
 // https://www.php.net/manual/en/ref.array.php
 
 // ============================================
@@ -92,13 +92,28 @@ echo '</pre>';
 // ============================================
 
 // Create an associative array
-
+$person = [
+    'name' => 'Abdul karim',
+    'surname' => 'bulbul',
+    'age' => 27,
+    'hobbies' => ['coding','video Gamming','cricket']
+];
+echo '<pre>';
+print_r($person);
+var_dump($person);
+echo '</pre>';
 // Get element by key
-
+echo $person['name'].'<br>';
 // Set element by key
-
+$person['channel'] = 'CleaverProgrammer';
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
 // Null coalescing assignment operator. Since PHP 7.4
-
+$person['address'] ??= 'unknown';
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
 // Check if array has specific key
 
 // Print the keys of the array
